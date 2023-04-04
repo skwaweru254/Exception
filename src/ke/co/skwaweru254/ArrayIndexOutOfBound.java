c3bd54bb9dbff83b77287618fad65fce6817e422
skwaweru254 <skwaweru254@gmail.com>
2023-04-04 15:15:28 +0300

    Runtime is unchecked exception

--- src/ke/co/skwaweru254/ArrayIndexOutOfBound.java
+++ src/ke/co/skwaweru254/ArrayIndexOutOfBound.java
@@ -2,7 +2,7 @@ package ke.co.skwaweru254;
 
 public class ArrayIndexOutOfBound {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
         try {
             int[] bae = new int[4];
             bae[5]  = 5;
@@ -11,5 +11,12 @@ public class ArrayIndexOutOfBound {
             System.out.println("ArrayIndexOutOfBound is the exception caught");
 
         }
+
+        //output all the runtime exception after compiling
+        throw new RuntimeException();
+
+        //throw new Exception(); for all checked before compiling
+
+
     }
 }
